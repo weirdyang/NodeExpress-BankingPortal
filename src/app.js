39e5f4955af394b834bc7cb9dd84249c5ab5cb93
weirdyang <wy4786@gmail.com>
2021-03-17 13:11:10 +0800

    module 3 completed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-
+const { F } = require('ramda');
+const debug = require('debug')('app:app');
 const app = express();
 
 const viewsPath = path.join(__dirname, 'views');
@@ -11,6 +12,10 @@ const staticPath = path.join(__dirname, 'public');
 app.use(express.static(staticPath));
 // https://github.com/mde/ejs/blob/master/docs/syntax.md
 app.set('view engine', 'ejs');
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // accounts
 const accountsPath = path.join(__dirname, 'json', 'accounts.json');
@@ -39,9 +44,42 @@ app.get('/credit', (req, res) => {
 });
 
 app.get('/profile', (req, res) => {
-  return res.render('profile',{ user: users[0]});
-})
+  return res.render('profile', { user: users[0] });
+});
 
+app.post('/transfer', (req, res) => {
+  const { from, to, amount } = req.body;
+
+  accounts[from].balance -= parseInt(amount);
+  accounts[to].balance += parseInt(amount);
+
+  const accountsJSON = JSON.stringify(accounts);
+  debug(accountsJSON);
+  fs.writeFileSync(accountsPath, accountsJSON, 'utf-8');
+  debug(req);
+  return res.render('transfer', { message: 'Transfer Completed' });
+});
+
+app.get('/transfer', (req, res) => {
+  return res.render('transfer');
+});
+
+app.get('/payment', (req, res) => {
+  return res.render('payment', { account: accounts.credit });
+});
+
+app.post('/payment', (req, res) => {
+  const { amount } = req.body;
+  debug(amount);
+  accounts.credit.balance -= parseInt(amount);
+  accounts.credit.available += parseInt(amount);
+  const accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(accountsPath, accountsJSON, 'utf-8');
+  return res.render('payment', {
+    message: 'Payment Successful',
+    account: accounts.credit,
+  });
+});
 app.listen(3000, () => {
   console.log('PS Project Running on port 3000!');
 });
